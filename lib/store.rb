c2f7b29cd46cacb2e5ceb876c63549351a2f3e62
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-08 18:41:27 -0500

    restrict users from deleting (aka destroying) store records for stores that have 1 or more employees

--- lib/store.rb
+++ lib/store.rb
@@ -6,9 +6,20 @@ class Store < ActiveRecord::Base
     greater_than_or_equal_to: 0}
   validate :apparel
 
+  before_destroy :store_has_employee?
+
   def apparel
     if !womens_apparel && !mens_apparel
       errors.add(:mens_apparel, "either mens or womens apparel must be true")
     end
   end
+
+  private
+  def store_has_employee?
+    if (self.employees.size >= 1)
+      self.errors.add(:base, "Can't be destroy because of something")
+      false
+    end
+  end
+
 end
