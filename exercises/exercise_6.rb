8f5100f24a3a4a9ab21ea080afaf9a38e9b9fac4
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-08 17:00:16 -0500

    Employee entries created

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Tim", last_name: "Walsh", hourly_rate: 70)
+
+@store1.employees.create(first_name: "Michel", last_name: "sha", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Felix", last_name: "linto", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Max", last_name: "Beck", hourly_rate: 80)
