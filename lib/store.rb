3bf79648519441798a3bcae9fac7dbfefabd3166
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-08 17:38:38 -0500

    Removed commented instructions

--- lib/store.rb
+++ lib/store.rb
@@ -12,8 +12,3 @@ class Store < ActiveRecord::Base
     end
   end
 end
-
-  
-  # * Stores must always have a name that is a minimum of 3 characters
-  # * Stores have an annual_revenue that is a number (integer) that must be 0 or more
-  # * BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
