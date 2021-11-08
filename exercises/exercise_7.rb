8b81f317560a5c7560bb2648eacbcb68b753cb54
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-08 17:37:49 -0500

    Test validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "please type store name"
+
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name, annual_revenue: "ss", mens_apparel: false, womens_apparel: false)
+new_store.valid?
+
+puts new_store.errors.messages
+
+
+puts "Employee name"
+employee = gets.chomp
+new_employee = Employee.new(first_name: employee, hourly_rate: 10)
+new_employee.valid?
+puts new_employee.errors.messages
